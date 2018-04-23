e01eb91859a9b27a998608b98b7c8634eebdfab5
Artnod <artnod78@users.noreply.github.com>
2018-04-23 16:40:36 +0200

    Add sensor reading
    Add DS18B20 temperature sensor reading

--- BonsaiProject/BonsaiProject.ino
+++ BonsaiProject/BonsaiProject.ino
@@ -1,16 +1,20 @@
+#include <EEPROM.h>
 #include <Wire.h>
 #include <RTClib.h>
-#include <EEPROM.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #include <LordPwmTimer.h>
 
 #define TIMEZONE 2            // UTC france = +2
 const float LATITUDE = 43.70; // Nice = latitude = 43.7000000
 const float LONGITUDE = 7.25; // Nice = longitude = 7.250000
-#define LIGHT_PIN 11          // pwm pin id for light
-#define PWM_TIME 900          // nb of sec for pwm (0 to 255)
+#define PWM_TIME 900          // Nombre de seconde pour passer le pwm de 0 a 255
+#define LIGHT_PIN 11          // Broche utilise pour la lumiere
+#define ONE_WIRE_BUS 4        // Broche utilise pour les sondes DS18B20
 
 // ### RTC ###
-RTC_DS1307 RTC;               // Tells the RTC library that we're using a DS1307 RTC
+RTC_DS1307 RTC;               // Instance DS1307 RTC
 DateTime now;                 // Variable pour recuperer l'heure de l'horloge
 int DST = 0;                  // Heure été/hiver
 void changeDST(void)          // Changement automatique de l'heure été / hiver
@@ -29,6 +33,15 @@ void changeDST(void)          // Changement automatique de l'heure été / hiver
   }
 }
 
+// ### DS18B20 ###
+#define TEMPERATURE_PRECISION 9
+OneWire oneWire(ONE_WIRE_BUS);        // Instance OneWire 
+DallasTemperature sensors(&oneWire);  // Instance Dallas Temperature a partir de l'instance OneWire.
+DeviceAddress insideThermometer = { 0x28, 0xEB, 0x75, 0x2D, 0x9, 0x0, 0x0, 0x13 };    // adresses des sondes DS18B20
+DeviceAddress outsideThermometer = { 0x28, 0xFF, 0xC3, 0x20, 0xA8, 0x15, 0x1, 0xCA }; // adresses des sondes DS18B20
+float insideTemp = 0;                 // Variable pour recuperer la temperature
+float outsideTemp = 0;                // Variable pour recuperer la temperature
+
 // ### Light ###
 LordPwmTimer light(LIGHT_PIN);
 
@@ -36,6 +49,7 @@ LordPwmTimer light(LIGHT_PIN);
 void setup ()
 {
   // affichage temporaire en Serial
+  // sera remplacer par l'affichage lcd
   Serial.begin(9600);
 
   // Initialize RTC
@@ -49,6 +63,10 @@ void setup ()
     EEPROM.put(0, DST);
   }
 
+  // Initialize DS18B20
+  sensors.setResolution(insideThermometer, TEMPERATURE_PRECISION);
+  sensors.setResolution(outsideThermometer, TEMPERATURE_PRECISION);
+  
   // Initialize light
   light.setLord(TIMEZONE, LATITUDE, LONGITUDE);
   light.setValue(LORDTIMER_PWM_TIME, PWM_TIME);
@@ -65,9 +83,12 @@ void loop ()
   // ***************** Running every sec ********************************
   if (now.second() != lastSec)
   {
+    insideTemp = sensors.getTempC(insideThermometer);
+    outsideTemp = sensors.getTempC(outsideThermometer);
     light.run(now);
     Serial.println("####################");
     printTime();
+    printTemp();
     printLight();
     lastSec = now.second();
   }
@@ -84,12 +105,20 @@ void printTime()
   Serial.print(" - Time: ");
   Serial.println((now.hour() * 60) + now.minute());
 }
+void printTemp()
+{
+  Serial.print("Temp1: ");
+  Serial.print(insideTemp);
+  Serial.print(" - Temp2: ");
+  Serial.println(outsideTemp);
+  
+}
 void printLight()
 {
   Serial.print("pwm: ");
-  Serial.println(light.getPwm());
+  Serial.print(light.getPwm());
   Serial.print(" - on: ");
-  Serial.println(light.getValue(LORDTIMER_ON));
+  Serial.print(light.getValue(LORDTIMER_ON));
   Serial.print(" - off: ");
   Serial.println(light.getValue(LORDTIMER_OFF));
 }
