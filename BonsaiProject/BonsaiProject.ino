2bc8446baf53c8cdd429d3ec77d47baa55f65851
Artnod <artnod78@users.noreply.github.com>
2018-04-24 16:41:12 +0200

    Change printLight()
    convert time in minute from midnight to time in hh:mm

--- BonsaiProject/BonsaiProject.ino
+++ BonsaiProject/BonsaiProject.ino
@@ -18,8 +18,8 @@ RTC_DS1307 RTC;               // Instance DS1307 RTC
 DateTime now;                 // Variable pour recuperer l'heure de l'horloge
 int DST = 0;                  // Heure été/hiver
 void changeDST(void)          // Changement automatique de l'heure été / hiver
-{  
-  if (now.dayOfTheWeek() == 0 && now.month() == 3 && now.day() >= 25 && now.day() <= 31 && now.hour() >= 2 && now.minute() >= 0 && now.second() >= 0 && DST == 0)
+{
+  if (now.dayOfTheWeek() == 0 && now.month() == 3 && now.day() >= 25 && now.day() <= 31 && now.hour() == 2 && now.minute() == 0 && now.second() == 0 && DST == 0)
   {
     RTC.adjust(DateTime(now.year(), now.month(), now.day(), now.hour() + 1, now.minute(), now.second()));
     DST = 1;
@@ -78,12 +78,12 @@ void loop ()
 {
   // ***************** Recuperation de l'heure ********************************
   now = RTC.now(); //get time from RTC
-  
+  changeDST(); // changement automatique de l'heure été / hiver
 
   // ***************** Running every sec ********************************
   if (now.second() != lastSec)
   {
-    changeDST(); // changement automatique de l'heure été / hiver
+    sensors.requestTemperatures();
     insideTemp = sensors.getTempC(insideThermometer);
     outsideTemp = sensors.getTempC(outsideThermometer);
     light.run(now);
@@ -99,12 +99,10 @@ void loop ()
 void printTime()
 {
   Serial.print(now.hour());
-  Serial.print(":");
+  Serial.print("h");
   Serial.print(now.minute());
   Serial.print(":");
-  Serial.print(now.second());
-  Serial.print(" - Time: ");
-  Serial.println((now.hour() * 60) + now.minute());
+  Serial.println(now.second());
 }
 void printTemp()
 {
@@ -116,10 +114,21 @@ void printTemp()
 }
 void printLight()
 {
+  int lordOn = light.getValue(LORDTIMER_ON);
+  int minuteOn = lordOn % 60;
+  int heureOn = (lordOn - minuteOn) /60;
+  int lordOff = light.getValue(LORDTIMER_OFF);
+  int minuteOff = lordOff % 60;
+  int heureOff = (lordOff - minuteOn) /60;
   Serial.print("pwm: ");
   Serial.print(light.getPwm());
   Serial.print(" - on: ");
-  Serial.print(light.getValue(LORDTIMER_ON));
-  Serial.print(" - off: ");
-  Serial.println(light.getValue(LORDTIMER_OFF));
+  Serial.print(heureOn);
+  Serial.print("h");
+  Serial.print(minuteOn);
+  Serial.print(":00 - off: ");
+  Serial.print(heureOff);
+  Serial.print("h");
+  Serial.print(minuteOff);
+  Serial.println(":00");
 }
